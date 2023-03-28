a4a4d15f9a15ca8b6802d575a4510e2f5f9196ce
JohannesWM <97265854+JohannesWM@users.noreply.github.com>
2023-03-28 15:10:28 -0400

    base_rotation_test2.py | test for usefulness of this code

--- base_rotation_test2.py
+++ base_rotation_test2.py
@@ -1,6 +1,24 @@
-from time import sleep
 import RPi.GPIO as GPIO
-from RpiMotorLib import RpiMotorLib
+from time import sleep
+
+# Direction pin from controller
+DIR = 10
+# Step pin from controller
+STEP = 8
+# 0/1 used to signify clockwise or counterclockwise.
+CW = 1
+CCW = 0
+
+# Setup pin layout on PI
+GPIO.setmode(GPIO.BOARD)
+
+# Establish Pins in software
+GPIO.setup(DIR, GPIO.OUT)
+GPIO.setup(STEP, GPIO.OUT)
+
+# Set the first direction you want it to spin
+GPIO.output(DIR, CW)
+
 try:
 	# Run forever.
 	while True:
