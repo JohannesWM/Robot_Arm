121f3556daa6316bd9614e7415596f4d1f81349e
JohannesWM <97265854+JohannesWM@users.noreply.github.com>
2023-05-10 09:25:31 -0400

    motor stays still, but active

---
+++ servo_rotation/servo_test_files/servo_rotation_2servos_syncTEST.py
@@ -0,0 +1,43 @@
+import RPi.GPIO as GPIO
+import time
+
+servoPIN = 5
+servo2PIN = 6
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servoPIN, GPIO.OUT)
+GPIO.setup(servo2PIN, GPIO.OUT)
+
+z = GPIO.PWM(servo2PIN, 50) # GPIO 17 for PWM with 50Hz
+z.start(2.5) # Initialization
+p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
+p.start(2.5) # Initialization
+try:
+  while True:
+    p.ChangeDutyCycle(5)
+    z.ChangeDutyCycle(5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(7.5)
+    z.ChangeDutyCycle(7.5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(10)
+    z.ChangeDutyCycle(10)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(12.5)
+    z.ChangeDutyCycle(12.5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(10)
+    z.ChangeDutyCycle(10)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(7.5)
+    z.ChangeDutyCycle(7.5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(5)
+    z.ChangeDutyCycle(5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(2.5)
+    z.ChangeDutyCycle(2.5)
+    time.sleep(0.5)
+except KeyboardInterrupt:
+  p.stop()
+  z.stop()
+  GPIO.cleanup()
